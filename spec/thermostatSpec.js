07ef334dcb0d4692f550662d87d3773e3ba73ab0
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-25 12:43:44 +0100

    Tested and implemented the Thermostat so that it can now increase temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,8 @@ describe("Thermostat", function() {
   it("starts at 20 degrees Celsius", function() {
     expect(thermostat.startTemp).toEqual(20);
   });
+  it("can increase temperature", function(){
+    thermostat.increaseTemp();
+    expect(thermostat.startTemp).toEqual(21);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,7 @@
 function Thermostat() {
   this.startTemp = 20;
   }
+
+Thermostat.prototype.increaseTemp = function () {
+  this.startTemp +=1;
+};
