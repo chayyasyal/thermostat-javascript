92cc0c63c8a1adec2b3a7806690947b6de2e0d76
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-26 16:45:51 +0100

    Tested and implemented a max temperature of 25 degrees when the thermostat is set to power save mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,26 +6,35 @@ describe("Thermostat", function(){
   });
 
   it("starts at 20 degrees Celsius", function(){
-    expect(thermostat.startTemp).toEqual(20);
+    expect(thermostat.getCurrentTemp).toEqual(20);
   });
   it("can increase temperature", function(){
     thermostat.increaseTemp();
-    expect(thermostat.startTemp).toEqual(21);
+    expect(thermostat.getCurrentTemp).toEqual(21);
   });
   it("can decrease temperature", function(){
     thermostat.decreaseTemp();
-    expect(thermostat.startTemp).toEqual(19);
+    expect(thermostat.getCurrentTemp).toEqual(19);
   });
 
   it("has a minimum temperature of 10 degrees Celsius", function(){
     for(var i = 0; i < 11; i++){
       thermostat.decreaseTemp();
     };
-    expect(thermostat.startTemp).toEqual(10);
+    expect(thermostat.getCurrentTemp).toEqual(10);
   });
   it("has a power saving mode on by default", function(){
     expect(thermostat.powerSaveModeOn).toBe(true);
   });
+  describe("when power save mode is on", function(){
+    it("has a max temp of 25 degrees Celsius", function(){
+      for (var i = 0; i < 5; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemp).toEqual(25);
+    });
+  });
+
   it("can switch power saving mode off", function(){
     expect(thermostat.powerSaveModeOff).toBe(false);
   });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,23 +1,32 @@
 function Thermostat(){
   this.MINIMUM_TEMP = 10;
-  this.startTemp = 20;
+  this.getCurrentTemp = 20;
   this.powerSaveModeOn = true;
   this.powerSaveModeOff = false;
+  this.upTemp = 25;
   }
 
+Thermostat.prototype.getCurrentTemp = function (){
+  return this.temp;
+};
+
 Thermostat.prototype.increaseTemp = function() {
-  this.startTemp +=1;
+  this.getCurrentTemp +=1;
 };
 
 Thermostat.prototype.decreaseTemp = function() {
-  if (this.startTemp === 10) {
+  if (this.getCurrentTemp === 10) {
     return this.MINIMUM_TEMP;
   };
-  this.startTemp -=1;
+  this.getCurrentTemp -=1;
 };
 Thermostat.prototype.powerSaveModeOn = function() {
   return this.powerSaveMode === true;
 };
-Thermostat.prototype.powerSaveModeOff = function () {
+Thermostat.prototype.powerSaveModeOff = function() {
   return this.powerSaveMode === false;
+};
+
+Thermostat.prototype.up = function() {
+    return this.getCurrentTemp +=1;
 };
